35e33c878a07d0ea708b95807453934a2be4894e
Tiago Rocha <tiagoarrocha@gmail.com>
2024-04-18 20:12:12 -0300

    fix: Ajuste no fav icon e na contagem regressiva

--- src/components/counter/index.tsx
+++ src/components/counter/index.tsx
@@ -19,33 +19,42 @@ export const CounterComponent = ({ targetDate }: CounterComponentProps) => {
   const calculateTimeLeft = () => {
     const difference = +new Date(targetDate) - +new Date();
     let timeLeft: TimeLeftProps = {};
-
-    if (difference > 0) {
-      timeLeft = {
-        days: {
-          text: "DIAS",
-          value: Math.floor(difference / (1000 * 60 * 60 * 24)).toString(),
-        },
-        hours: {
-          text: "HORAS",
-          value: Math.floor((difference / (1000 * 60 * 60)) % 24)
-            .toString()
-            .padStart(2, "0"),
-        },
-        minutes: {
-          text: "MINUTOS",
-          value: Math.floor((difference / 1000 / 60) % 60)
-            .toString()
-            .padStart(2, "0"),
-        },
-        seconds: {
-          text: "SEGUNDOS",
-          value: Math.floor((difference / 1000) % 60)
-            .toString()
-            .padStart(2, "0"),
-        },
-      };
-    }
+    const days = difference / (1000 * 60 * 60 * 24);
+    const hours = (difference / (1000 * 60 * 60)) % 24;
+    const minutes = (difference / 1000 / 60) % 60;
+    const seconds = (difference / 1000) % 60;
+    timeLeft = {
+      days: {
+        text: "DIAS",
+        value:
+          days > 0
+            ? Math.floor(difference / (1000 * 60 * 60 * 24)).toString()
+            : "0",
+      },
+      hours: {
+        text: "HORAS",
+        value:
+          hours > 0
+            ? Math.floor((difference / (1000 * 60 * 60)) % 24)
+                .toString()
+                .padStart(2, "0")
+            : "0",
+      },
+      minutes: {
+        text: "MINUTOS",
+        value:
+          minutes > 0
+            ? Math.floor((difference / 1000 / 60) % 60)
+                .toString()
+                .padStart(2, "0")
+            : "0",
+      },
+      seconds: {
+        text: "SEGUNDOS",
+        value:
+          seconds > 0 ? Math.floor(seconds).toString().padStart(2, "0") : "0",
+      },
+    };
 
     return timeLeft;
   };
@@ -72,7 +81,7 @@ export const CounterComponent = ({ targetDate }: CounterComponentProps) => {
     );
   });
 
-  return (
+  return timerComponents.length ? (
     <article className="flex flex-col gap-8 items-center">
       <h2 className="text-2xl md:text-5xl font-bold">Contagem regressiva</h2>
       <div className="grid grid-flow-row grid-cols-4 gap-2 md:gap-4">
@@ -83,5 +92,5 @@ export const CounterComponent = ({ targetDate }: CounterComponentProps) => {
         )}
       </div>
     </article>
-  );
+  ) : null;
 };
