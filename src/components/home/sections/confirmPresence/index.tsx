cf556921b7d66536588ab9492b9e03a85daa2c1d
Tiago Rocha <tiagorocha@portalctech.com.br>
2024-05-20 13:25:28 -0300

    fix(images): removendo dependencia desnecessária

--- src/components/home/sections/confirmPresence/index.tsx
+++ src/components/home/sections/confirmPresence/index.tsx
@@ -116,7 +116,7 @@ export const ConfirmPresenceSection = ({
         section.scrollIntoView();
       }
     }
-  }, [imagesLoaded, window.location]);
+  }, [imagesLoaded]);
 
   return (
     <section className="odd:bg-gray-500 even:bg-white odd:text-white py-20 px-4 md:px-0" id="confirm-presence-section">
